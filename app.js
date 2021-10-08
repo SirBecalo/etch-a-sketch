44da79e806dfb02b5ccc2ac0dca54fecea60e8a8
ahmad diaa <ahmaddiaa1603@gmail.com>
2021-10-08 18:53:33 +0200

    made auto grid and div assignment

--- app.js
+++ app.js
@@ -5,22 +5,36 @@ let middle = document.querySelector("#middle");
 let footer = document.querySelector("#foot");
 //document vars
 
-let size = 16
+let size = prompt("size of grid? ")
 
-
-//function for making the grid
-function makeGrid(gridSize) {
-    if (gridSize <= 0) return;
-    let oneDiv = document.createElement('div');
-    oneDiv.id = gridSize
-    middle.appendChild(oneDiv);
-    makeGrid(gridSize-1);
+function makeBoxes(size) {
+    let repeatString = "repeat(" + size + ", 1fr)"
+    console.log(repeatString);
+    middle.style.gridTemplateColumns = repeatString;
+    middle.style.gridTemplateRows = repeatString;
+ 
+    return; 
     
 }
 
-//grid size
-makeGrid(size);
-setGrid(size);
+function setDivs(size) {
 
-function setGrid(gridSize) {
+    
+    
+    for (let i = 1; i <= size; i++) {
+        for (let j = 1; j <= size; j++) {
+            let oneDiv = document.createElement("div");
+            oneDiv.style.height = "100%";
+            oneDiv.style.width = "100%";
+            oneDiv.id = `${i} , ${j}`;
+            oneDiv.style.gridRow = `${i}/${i+1}`;
+            oneDiv.style.gridRow = `${j}/${j+1}`;
+            middle.appendChild(oneDiv);
+        }
+
+    }
 }
+
+
+makeBoxes(size);
+setDivs(size);
