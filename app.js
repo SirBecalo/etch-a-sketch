c0db47fcb9ec251b0905d7c76b678f9e58b010ec
ahmad diaa <ahmaddiaa1603@gmail.com>
2022-07-25 16:43:16 +0200

    commented old, trying <table>

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 //palette: EEF4D4 DAEFB3 D1B1CB 2E2F2F 051014
 const sketch = document.querySelector(".sketch") //where the magic happens!
-const num = 6
-const dimension = 50/num
+
+//sheinanigans to be understood
+// const num = 6
+// const dimension = 50/num
+
 const box = document.createElement('div') //the template for a pixel
 box.style.border = '1px solid lightgray'
 box.style.width = `${dimension}%`
 box.style.height = `${dimension}%`
 box.classList.add("box")
+// old, folded
+// for (let i = 0; i < num * num; i++) {
+//     sketch.appendChild(box.cloneNode(true))
+// }
 
-for (let i = 0; i < num * num; i++) {
-    sketch.appendChild(box.cloneNode(true))
-    
-}
+let gridSize = 6
 
-
+//trying to place pixels with table element
 
