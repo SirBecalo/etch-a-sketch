54768651d7703e863f21bf3b0fca3f2822ff11c0
ahmad diaa <ahmaddiaa1603@gmail.com>
2022-08-06 20:36:43 +0200

    looped variable changed to size from incorrect "gridsize"

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 //palette: EEF4D4 DAEFB3 D1B1CB 2E2F2F 051014
-const table = document.querySelector(".table") //where the magic happens!
+
 
 //sheinanigans to be understood
 // const num = 6
 // const dimension = 50/num
 
 //table templates
-const tr = document.createElement('tr')
-const td = document.createElement('td')
+const table = document.getElementById("table") //where the magic happens!
+const tr = document.createElement("tr")
+const td = document.createElement("td")
 
 //the template for a pixel
-const box = document.createElement('div')
-box.style.border = '1px solid lightgray'
-box.style.width = `${dimension}%`
-box.style.height = `${dimension}%`
-box.classList.add("box")
+// const box = document.createElement("div")
+// box.style.border = '1px solid lightgray'
+// box.style.width = `${dimension}%`
+// box.style.height = `${dimension}%`
+// box.classList.add("box")
 // old, folded
 // for (let i = 0; i < num * num; i++) {
 //     table.appendChild(box.cloneNode(true))
@@ -23,14 +24,19 @@ box.classList.add("box")
 //trying to place pixels with table element
 let gridSize = 6
 
-
-for (let horizontal = 0; horizontal < gridSize; horizontal++) {
-    const row = tr.className.add(horizontal.toString())
-    for (let vertical = 0; vertical < gridSize; vertical++) {
-        const col = td.className.add(vertical.toString())
+function fillTable(size){
+for (let horizontal = 0; horizontal < size; horizontal++) {
+    let row = tr
+    row.className = horizontal.toString()
+    table.appendChild(row)
+    for (let vertical = 0; vertical < size; vertical++) {
+        let col = td
+        col.className = vertical.toString()
         row.appendChild(col)
 
     }
-    table.appendChild(row)
- }
     
+ }  
+}
+    
+fillTable(gridSize)
