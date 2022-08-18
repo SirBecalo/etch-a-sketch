435a3062ba7d16fb7bf04902b0e5006d62a20ee2
ahmad diaa <ahmaddiaa1603@gmail.com>
2022-08-18 12:57:48 +0200

    got sketch event listener to work

--- app.js
+++ app.js
@@ -4,26 +4,36 @@
 
 const sketch = document.querySelector(".sketch") //where the magic happens!
 
-let gridSize = 6
+let gridSize = 6 //size of grid per side
 
-function fillTable(size){
+function createCanvas(size){
+
+    //nested for loops, one to create cols, one to fill each col with pixels
     for (let cols = 0; cols <= size; cols++) {
-        const col = document.createElement("div") //1 div,  1 col!
-        col.id = cols //gotta name them all!
-        col.classList.add("col")
+        const col = document.createElement("div") //1 div = 1 col!
+        col.id = `col${cols}` //gotta name them all! IDing in numerical order
+        col.classList.add("col") //aggregate for easy styleing
         sketch.appendChild(col) //gotta dispaly them all!
+
         for (let pixels = 0; pixels <= size; pixels++) {
-            const pixel = document.createElement("div") //1 div,  1 col!
-            pixel.id = pixels //gotta name them all!
-            pixel.classList.add("pixel")
+            const pixel = document.createElement("div") //1 div = 1 pixel!
+            pixel.id = `col${cols}pixel${pixels}` //gotta name them all numerical order! 
+            pixel.classList.add("pixel") //aggregate for easy styling
             col.appendChild(pixel) //gotta dispaly them all!
-
-
 
         }  
     }
 }
+// changes element color when called by listener
+//to scale this up i think classlist editing will be more appropriate.
 
+
+sketch.onmouseover = function (event) {
+    let toChange = event.target.closest('.pixel')
+    console.log(toChange)
+    toChange.style.backgroundColor = "black"
     
-fillTable(gridSize)
+}
+// sketch.addEventListener(onmouseover, changeColorBlack(sketch), false)
+createCanvas(gridSize)
 
